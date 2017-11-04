94ed37e19f84fceb38de011bb18033a8ebb76705
hrabovszki2310 <zh@openkeyword.de>
2017-11-04 06:56:59 +0100

    travis try 10

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -122,6 +122,8 @@ if [ -d "html" ] && [ -f "html/index.html" ]; then
     # Force push to the remote gh-pages branch.
     # The ouput is redirected to /dev/null to hide any sensitive credential data
     # that might otherwise be exposed.
+    echo '----------------'
+    echo '>> push --force "https://' ${GH_REPO_TOKEN}'@'${GH_REPO_REF}'" > /dev/null 2>&1'
     git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
 else
     echo '' >&2
