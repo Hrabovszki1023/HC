5f24b30286c7a2d9a170990cdb7578d9235e940a
hrabovszki2310 <zh@openkeyword.de>
2017-11-04 05:24:09 +0100

    travis try 8

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -71,9 +71,13 @@ echo "" > .nojekyll
 
 ################################################################################
 ##### Generate the Doxygen code documentation and log the output.          #####
+echo '================================================='
 echo 'Generating Doxygen code documentation...'
+echo '================================================='
 # Redirect both stderr and stdout to the log file AND the console.
 $HOME/local/bin/doxygen $DOXYFILE # 2>&1 | tee doxygen.log
+pwd
+ls -sl
 
 ################################################################################
 ##### Upload the documentation to the gh-pages branch of the repository.   #####
