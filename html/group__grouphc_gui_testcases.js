6e76692c5f5b911b3f075d9fd8241798686bfc67
Travis CI <travis@travis-ci.org>
2017-11-05 13:04:27 +0000

    Deploy code docs to GitHub Pages Travis build: 22
    Commit: c208663dedc6cb8e3375ed9f7c5c1f566bb1159f

--- html/group__grouphc_gui_testcases.js
+++ html/group__grouphc_gui_testcases.js
@@ -1,5 +1,26 @@
 var group__grouphc_gui_testcases =
 [
+    [ "hcButtonWithCountLabel_Chrome_Test", "classhc_1_1testcases_1_1hc_button_with_count_label_1_1hc_button_with_count_label___chrome___test.html", [
+      [ "FirefoxAfter", "classhc_1_1testcases_1_1hc_button_with_count_label_1_1hc_button_with_count_label___chrome___test.html#aed7c07790ab56285b21b92079ecfd1b4", null ]
+    ] ],
+    [ "hcButtonWithCountLabel_Firefox_Test", "classhc_1_1testcases_1_1hc_button_with_count_label_1_1hc_button_with_count_label___firefox___test.html", [
+      [ "FirefoxAfter", "classhc_1_1testcases_1_1hc_button_with_count_label_1_1hc_button_with_count_label___firefox___test.html#a7001044fa649b8a994b8e407be104c44", null ]
+    ] ],
+    [ "hcButtonWithCountLabel_HTMLUnitDriver_Test", "classhc_1_1testcases_1_1hc_button_with_count_label_1_1hc_button_with_count_label___h_t_m_l_unit_driver___test.html", null ],
+    [ "hcButtonWithCountLabel_Test", "classhc_1_1testcases_1_1hc_button_with_count_label_1_1hc_button_with_count_label___test.html", [
+      [ "tcVerifyCaption", "classhc_1_1testcases_1_1hc_button_with_count_label_1_1hc_button_with_count_label___test.html#a33b45fba77a5661e6af5ee1fc41f5eef", null ],
+      [ "tcVerifyExists", "classhc_1_1testcases_1_1hc_button_with_count_label_1_1hc_button_with_count_label___test.html#a3cf4f2f0a658a53d20e9b20c05ada2c1", null ],
+      [ "tcVerifyLabel", "classhc_1_1testcases_1_1hc_button_with_count_label_1_1hc_button_with_count_label___test.html#ab6467c0e8b84dd45929a17f850d8a821", null ],
+      [ "myMem", "classhc_1_1testcases_1_1hc_button_with_count_label_1_1hc_button_with_count_label___test.html#a6bbac169fc56b43f425a1e3d86cad747", null ],
+      [ "name", "classhc_1_1testcases_1_1hc_button_with_count_label_1_1hc_button_with_count_label___test.html#acfba45449129763512a35e44615a596c", null ]
+    ] ],
+    [ "hcEmoji_Chrome_Test", "classhc_1_1testcases_1_1hc_emoji_1_1hc_emoji___chrome___test.html", [
+      [ "FirefoxAfter", "classhc_1_1testcases_1_1hc_emoji_1_1hc_emoji___chrome___test.html#a74d219fbec4038d038dcfd254005c6ee", null ]
+    ] ],
+    [ "hcEmoji_Firefox_Test", "classhc_1_1testcases_1_1hc_emoji_1_1hc_emoji___firefox___test.html", [
+      [ "FirefoxAfter", "classhc_1_1testcases_1_1hc_emoji_1_1hc_emoji___firefox___test.html#a26b2cdd7f51d8e8c6b8a15ed36802c44", null ]
+    ] ],
+    [ "hcEmoji_HTMLUnitDriver_Test", "classhc_1_1testcases_1_1hc_emoji_1_1hc_emoji___h_t_m_l_unit_driver___test.html", null ],
     [ "hcEmoji_Test", "classhc_1_1testcases_1_1hc_emoji_1_1hc_emoji___test.html", [
       [ "tcLogExists", "classhc_1_1testcases_1_1hc_emoji_1_1hc_emoji___test.html#a117ecbdecec3ad3470d77b0077dde9b5", null ],
       [ "myMem", "classhc_1_1testcases_1_1hc_emoji_1_1hc_emoji___test.html#a218605edba8e57efb463ee1903c5333f", null ],
