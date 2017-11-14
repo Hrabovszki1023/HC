e0e416ee8ebe82e03c91af51be9042f9a0eb4735
Travis CI <travis@travis-ci.org>
2017-11-14 16:54:38 +0000

    Deploy code docs to GitHub Pages Travis build: 36
    Commit: 1fafd5bd71898d3c7e3cb5ce0843eaf38f971730

--- html/group__grouphc_gui_testcases.js
+++ html/group__grouphc_gui_testcases.js
@@ -9,6 +9,8 @@ var group__grouphc_gui_testcases =
     [ "hcLogin_HTMLUnitDriver_Test", "classhc_1_1testcases_1_1hc_1_1hc_login_1_1hc_login___h_t_m_l_unit_driver___test.html", null ],
     [ "hcLogin_Test", "classhc_1_1testcases_1_1hc_1_1hc_login_1_1hc_login___test.html", [
       [ "tcLogExists", "classhc_1_1testcases_1_1hc_1_1hc_login_1_1hc_login___test.html#a68bd0b51f85c81cd87df641366029482", null ],
+      [ "tcLoginInvalidUser", "classhc_1_1testcases_1_1hc_1_1hc_login_1_1hc_login___test.html#abe7519917a8da93b2f3b6a73f3d06b9d", null ],
+      [ "tcLoginValidUser", "classhc_1_1testcases_1_1hc_1_1hc_login_1_1hc_login___test.html#a5e2873f02b03d7b41d5552a8b95b7abf", null ],
       [ "myMem", "classhc_1_1testcases_1_1hc_1_1hc_login_1_1hc_login___test.html#a56fcfaab1fdbaa04b7cda3178fb940fa", null ],
       [ "name", "classhc_1_1testcases_1_1hc_1_1hc_login_1_1hc_login___test.html#af2c4d0a998ed394e48e9f678f317a486", null ]
     ] ],
--- html/search/functions_2.js
+++ html/search/functions_2.js
@@ -1,6 +1,8 @@
 var searchData=
 [
   ['tclogexists',['tcLogExists',['../classhc_1_1testcases_1_1hc_1_1hc_login_1_1hc_login___test.html#a68bd0b51f85c81cd87df641366029482',1,'hc.testcases.hc.hcLogin.hcLogin_Test.tcLogExists()'],['../classhc_1_1testcases_1_1hcguicomponent_1_1hc_emoji_1_1hc_emoji___test.html#aa25de7cfe97e94e4b15279dfb5640619',1,'hc.testcases.hcguicomponent.hcEmoji.hcEmoji_Test.tcLogExists()']]],
+  ['tclogininvaliduser',['tcLoginInvalidUser',['../classhc_1_1testcases_1_1hc_1_1hc_login_1_1hc_login___test.html#abe7519917a8da93b2f3b6a73f3d06b9d',1,'hc::testcases::hc::hcLogin::hcLogin_Test']]],
+  ['tcloginvaliduser',['tcLoginValidUser',['../classhc_1_1testcases_1_1hc_1_1hc_login_1_1hc_login___test.html#a5e2873f02b03d7b41d5552a8b95b7abf',1,'hc::testcases::hc::hcLogin::hcLogin_Test']]],
   ['tcverifycaption',['tcVerifyCaption',['../classhc_1_1testcases_1_1hcguicomponent_1_1hc_button_with_count_label_1_1hc_button_with_count_label___test.html#aa46ebbaa65eae26e35cf6accabe992af',1,'hc::testcases::hcguicomponent::hcButtonWithCountLabel::hcButtonWithCountLabel_Test']]],
   ['tcverifyexists',['tcVerifyExists',['../classhc_1_1testcases_1_1hcguicomponent_1_1hc_button_with_count_label_1_1hc_button_with_count_label___test.html#aa45ca7648defca20f1ef3ff97626cbdc',1,'hc::testcases::hcguicomponent::hcButtonWithCountLabel::hcButtonWithCountLabel_Test']]],
   ['tcverifylabel',['tcVerifyLabel',['../classhc_1_1testcases_1_1hcguicomponent_1_1hc_button_with_count_label_1_1hc_button_with_count_label___test.html#a74bedb303a3e600d59c90bb0a8edbfe6',1,'hc::testcases::hcguicomponent::hcButtonWithCountLabel::hcButtonWithCountLabel_Test']]],
